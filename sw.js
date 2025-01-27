70bd04be3c62dbbd8c3e687af32b666bdb0f196d
kobutri <tristan.kobusch@gmail.com>
2025-01-27 14:15:29 +0000

    Deploying to gh-pages from @ kobutri/egui-vs-react-perf@33fe502d7e8a6eda9ba08a590288199b02f24a64 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'react-vs-egui-performance-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './react-vs-egui-performance.js',
+  './react-vs-egui-performance_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
